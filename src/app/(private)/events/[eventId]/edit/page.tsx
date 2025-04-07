304fbc89c03117793d2962b99fd1627bab98c28a
“Iris <liuirisny@gmail.com>
2025-04-07 12:25:20 -0400

    fixes vercel deployment env error

--- src/app/(private)/events/[eventId]/edit/page.tsx
+++ src/app/(private)/events/[eventId]/edit/page.tsx
@@ -7,9 +7,11 @@ import { notFound } from "next/navigation";
 export const revalidate = 0 /* you are telling Next.js to revalidate the page data on every request. This means that instead of serving cached data, the server will always fetch fresh data from the source. */
 
 export default async function EditEventPage({ 
-    params
+    params,
+    searchParams
 }: {
-    params: Promise<{ eventId: string }> 
+    params: Promise<{ eventId: string }>
+    searchParams?: { [key: string]: string | string[] | undefined }
 }) { /* [eventId allows to take in dynamic parameters with the same name] */
     const { userId, redirectToSignIn } = await auth()
     const { eventId } = await params
